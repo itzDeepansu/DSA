edc44c6b518368eb38930daa132255f43f5aafa1
Deepansu Siwach <150233158+itzDeepansu@users.noreply.github.com>
2025-07-06 17:39:29 +0530

    Time: 5 ms (12.35%) | Memory: 17 MB (61.44%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -2,26 +2,34 @@ class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
         stack<int> numbers;
-
-        for (const string& token : tokens) {
-            if (token == "+" || token == "-" || token == "*" || token == "/") {
+        for (auto token : tokens) {
+            if (token == "+") {
                 int last = numbers.top();
                 numbers.pop();
                 int secondLast = numbers.top();
                 numbers.pop();
-                if (token == "+")
-                    numbers.push(secondLast + last);
-                else if (token == "-")
-                    numbers.push(secondLast - last);
-                else if (token == "*")
-                    numbers.push(secondLast * last);
-                else if (token == "/")
-                    numbers.push(secondLast / last);
-            } else {
+                numbers.push(secondLast + last);
+            } else if (token == "-") {
+                int last = numbers.top();
+                numbers.pop();
+                int secondLast = numbers.top();
+                numbers.pop();
+                numbers.push(secondLast - last);
+            } else if (token == "*") {
+                int last = numbers.top();
+                numbers.pop();
+                int secondLast = numbers.top();
+                numbers.pop();
+                numbers.push(secondLast * last);
+            } else if (token == "/") {
+                int last = numbers.top();
+                numbers.pop();
+                int secondLast = numbers.top();
+                numbers.pop();
+                numbers.push(secondLast / last);
+            } else
                 numbers.push(stoi(token));
-            }
         }
-
         return numbers.top();
     }
 };
